e13a87942b033fc95b4d1763bd6a9dac6bd73b0b
unknown <chelsafv75@gmail.com>
2022-11-27 20:39:30 +0700

    add owl-carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <HeaderComponent />
     <!-- Hero Section Begin -->
-    <div class="hero-section">
-        <Carousel class="hero-items">
-            <Slide class="single-hero-items set-bg" style="background-image: url('img/hero-1.jpg')">
+    <section class="hero-section">
+        <carousel class="hero-items" :items="1" :nav="false">
+            <div class="single-hero-items set-bg" style="background-image: url('img/hero-1.jpg')">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
@@ -17,8 +17,8 @@
                         </div>
                     </div>
                 </div>
-            </Slide>
-            <Slide class="single-hero-items set-bg" style="background-image: url('img/hero-2.jpg')">
+            </div>
+            <div class="single-hero-items set-bg" style="background-image: url('img/hero-2.jpg')">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
@@ -31,12 +31,9 @@
                         </div>
                     </div>
                 </div>
-            </Slide>
-            <template #addons>
-              <Pagination />
-            </template>
-        </Carousel>
-    </div>
+            </div>
+        </carousel>
+    </section>
     <!-- Hero Section End -->
   </div>
 </template>
@@ -45,16 +42,13 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue';
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination} from 'vue3-carousel'
+import carousel from 'vue-owl-carousel'
 
 export default {
   name: 'HomeView',
   components: {
     HeaderComponent,
-    Carousel,
-    Slide,
-    Pagination,
+    carousel,
   }
 }
 </script>
