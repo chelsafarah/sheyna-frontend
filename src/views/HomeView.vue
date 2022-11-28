b9e94fa3478a68aabf1ae14112b8eb23d9155b2e
unknown <chelsafv75@gmail.com>
2022-11-28 10:31:05 +0700

    fixing carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <HeaderComponent />
     <!-- Hero Section Begin -->
-    <div class="hero-section">
-        <Carousel class="hero-items">
+    <section class="hero-section">
+        <Carousel :items-to-show="1" :autoplay="2000" :wrap-around="true">
             <Slide class="single-hero-items set-bg" style="background-image: url('img/hero-1.jpg')">
                 <div class="container">
                     <div class="row">
@@ -32,11 +32,26 @@
                     </div>
                 </div>
             </Slide>
+            <Slide class="single-hero-items set-bg" style="background-image: url('img/hero-3.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
             <template #addons>
+              <Navigation />
               <Pagination />
             </template>
         </Carousel>
-    </div>
+    </section>
     <!-- Hero Section End -->
   </div>
 </template>
@@ -46,7 +61,7 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination} from 'vue3-carousel'
+import { Carousel, Slide, Pagination, Navigation} from 'vue3-carousel'
 
 export default {
   name: 'HomeView',
@@ -55,6 +70,15 @@ export default {
     Carousel,
     Slide,
     Pagination,
+    Navigation
   }
 }
 </script>
+
+<style>
+.set-bg {
+    width: 100%;
+    height: 500px;
+}
+</style>
+
